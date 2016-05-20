d680a4c34439babb2da50e05c3e69fd9c9689277
Dayi Wang <dayi.wang@gmail.com>
2016-05-20 14:39:26 +0800

    Fix "jobb -dump -d" unable to extract files
    1. Fix The Exception:
    Exception in thread "main" java.nio.BufferOverflowException
    	at java.nio.HeapByteBuffer.put(HeapByteBuffer.java:183)
    	at java.nio.ByteBuffer.put(ByteBuffer.java:832)
    	at com.android.jobb.EncryptedBlockFile$EncryptedBlockFileChannel.readDecryptedSector(EncryptedBlockFile.java:292)
    	at com.android.jobb.EncryptedBlockFile$EncryptedBlockFileChannel.read(EncryptedBlockFile.java:142)
    	at de.waldheinz.fs.util.FileDisk.read(FileDisk.java:118)
    	at de.waldheinz.fs.fat.ClusterChain.readData(ClusterChain.java:214)
    	at de.waldheinz.fs.fat.FatFile.read(FatFile.java:126)
    	at com.android.jobb.Main.dumpDirectory(Main.java:137)
    	at com.android.jobb.Main.dumpDirectory(Main.java:112)
    	at com.android.jobb.Main.main(Main.java:315)
    
    2.  Fix the FsFile read position error.
    
    issues:
    https://code.google.com/p/android/issues/detail?id=61344
    
    Change-Id: I85f1526c071ebe0467ec07fefd0e26cad537e5ba
    Signed-off-by: Dayi Wang <dayi.wang@gmail.com>

--- jobb/src/main/java/com/android/jobb/Main.java
+++ jobb/src/main/java/com/android/jobb/Main.java
@@ -130,7 +130,7 @@ public class Main {
                             FileChannel outputChannel = fos.getChannel();
                             int capacity = sTempBuf.capacity();
                             long length = f.getLength();
-                            for ( long pos = 0; pos < length; pos++ ) {
+                            for ( long pos = 0; pos < length;) {
                                 int readLength = (int)(length-pos > capacity ? capacity : length-pos);
                                 sTempBuf.rewind();
                                 sTempBuf.limit(readLength);
