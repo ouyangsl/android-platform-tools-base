dbb8d2d9b432dd50213bbd60d75144f39bce93f2
Amy Gu <amygu@google.com>
2021-10-06 17:53:21 +0000

    Add link to explanation about how dependency info file is used by Play.
    Change-Id: I668448f817e4e4218ef482c24372ad39bce15366

--- build-system/gradle-api/src/main/java/com/android/build/api/dsl/DependenciesInfo.kt
+++ build-system/gradle-api/src/main/java/com/android/build/api/dsl/DependenciesInfo.kt
@@ -18,8 +18,13 @@ package com.android.build.api.dsl
 
 import org.gradle.api.Incubating
 
-/** DSL object to specify whether to include SDK dependency information in APKs and Bundles. */
-@Incubating
+/** DSL object to specify whether to include SDK dependency information in APKs and Bundles.
+ * Including dependency information in your APK or Bundle allows Google Play to ensure that
+ * any third-party software your app uses complies with
+ * <a href="https://support.google.com/googleplay/android-developer/topic/9858052">Google Play's Developer Program Policies</a>.
+ * For more information, see the Play Console support page
+ * <a href="https://support.google.com/googleplay/android-developer/answer/10358880">Using third-party SDKs in your app</a>. */
+
 interface DependenciesInfo {
 
   /** If false, information about SDK dependencies of an APK will not be added to its signature
