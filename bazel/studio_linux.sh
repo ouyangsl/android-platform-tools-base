aba2401dc2327d2243d9dcc767ab91a18c4a6093
Julien Desprez <jdesprez@google.com>
2022-09-26 16:58:27 +0000

    Export annotations & common jar from studio
    They will be used to update platform prebuilts
    
    Change-Id: Ie6042b5cd26ca76fe82a549984913098c2e9f170
    Test: presubmit
    Bug: 235237215

--- bazel/studio_linux.sh
+++ bazel/studio_linux.sh
@@ -54,6 +54,8 @@ function copy_bazel_artifacts() {(
   cp -a ${bin_dir}/tools/base/dynamic-layout-inspector/skia/skiaparser.zip ${artifacts_dir}
   cp -a ${bin_dir}/tools/base/sdklib/commandlinetools_*.zip ${artifacts_dir}
   cp -a ${bin_dir}/tools/base/ddmlib/tools.ddmlib.jar ${artifacts_dir}/ddmlib.jar
+  cp -a ${bin_dir}/tools/base/annotations/annotations.jar ${artifacts_dir}/annotations.jar
+  cp -a ${bin_dir}/tools/base/common/tools.common.jar ${artifacts_dir}/tools.common.jar
   cp -a ${bin_dir}/tools/base/ddmlib/libincfs.jar ${artifacts_dir}
   cp -a ${bin_dir}/tools/base/lint/libs/lint-tests/lint-tests.jar ${artifacts_dir}
   cp -a ${bin_dir}/tools/base/deploy/deployer/deployer.runner_deploy.jar ${artifacts_dir}/deployer.jar
