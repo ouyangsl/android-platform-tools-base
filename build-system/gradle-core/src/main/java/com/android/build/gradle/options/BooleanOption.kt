f8e83413dff9bff901c1d76c1776ff912a588b2b
Scott Pollom <spollom@google.com>
2022-09-26 19:53:36 -0700

    Add android.lint.printStackTrace to BooleanOption
    Setting android.lint.printStackTrace=true in gradle.properties has the
    same effect as setting environment variable LINT_PRINT_STACKTRACE=true.
    
    Fixes: 237820036
    Test: modified existing
    Change-Id: I39f9063be404ca7113b178b3208ce6638f7f4469

--- build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/options/BooleanOption.kt
@@ -212,6 +212,8 @@ enum class BooleanOption(
     PRIVACY_SANDBOX_SDK_SUPPORT("android.experimental.privacysandboxsdk.enable", false, FeatureStage.Experimental),
 
     BUILD_ANALYZER_TASK_LABELS("android.experimental.buildanalyzer.tasklabels.enable", false, FeatureStage.Experimental),
+
+    PRINT_LINT_STACK_TRACE("android.lint.printStackTrace", false, FeatureStage.Experimental),
     /* ------------------------
      * SOFTLY-ENFORCED FEATURES
      */
