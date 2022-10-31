1251757e4e6a88813f042f13008b2fdd24938250
Liu Cunyuan <liucunyuan.lcy@linux.alibaba.com>
2022-10-31 10:03:30 +0800

    Add riscv64 support for gradle plugin
    Change-Id: I56d7f6a4a7cbf75fd7af33ad4962f71466e1cc8b
    Signed-off-by: Liu Cunyuan <liucunyuan.lcy@linux.alibaba.com>
    Signed-off-by: Mao Han <han_mao@linux.alibaba.com>

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -505,6 +505,7 @@ public final class SdkConstants {
     public static final String ABI_INTEL_ATOM64 = "x86_64";
     public static final String ABI_MIPS = "mips";
     public static final String ABI_MIPS64 = "mips64";
+    public static final String ABI_RISCV64 = "riscv64";
     /** Name of the CPU arch to support. */
     public static final String CPU_ARCH_ARM = "arm";
 
@@ -514,6 +515,7 @@ public final class SdkConstants {
     public static final String CPU_ARCH_MIPS = "mips";
     /** TODO double-check this is appropriate value for mips64 */
     public static final String CPU_ARCH_MIPS64 = "mips64";
+    public static final String CPU_ARCH_RISCV64 = "riscv64";
     /** Name of the CPU model to support. */
     public static final String CPU_MODEL_CORTEX_A8 = "cortex-a8";
 
