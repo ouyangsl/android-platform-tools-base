f1ac526247e0e12f3e2b9f25702816030d1214d9
Luke Edgar <lukeedgar@google.com>
2022-12-06 14:47:18 +0000

    Set Kotlin compiler output as jar in ExtractSdkShimLibraryTransform
    Fixes an issue on windows where the ExtractSdkShimLibraryTransform
    cleans tmp directories after the Kotlin compiler has run, however the
    files may still be locked. The Kotlin compiler will now produce a Jar
    rather than a directory to ensure, the files can be safely deleted.
    
    Bug: 261573698
    Test: PrivacySandboxSdkTest
    Change-Id: I25977977d1dd2be6e7b35d0f9d3de06e791a9138

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dependency/ExtractSdkShimTransform.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/dependency/ExtractSdkShimTransform.kt
@@ -37,11 +37,8 @@ import java.io.File
 import java.io.IOException
 import java.net.URL
 import java.net.URLClassLoader
-import java.nio.file.AccessDeniedException
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.logging.Level
-import java.util.logging.Logger
 import java.util.stream.Collectors
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
@@ -112,7 +109,7 @@ abstract class ExtractSdkShimTransform : TransformAction<ExtractSdkShimTransform
                             "Privacy Sandbox Sdk modules require at least one service declaration.")
         }
         try {
-            val kotlinOutDir = File(tempDirForApiGeneratorOutputs.toFile(), "kotlin-compiled")
+            val kotlinOutJar = File(tempDirForApiGeneratorOutputs.toFile(), "kotlin-compiled.jar")
             val javaOutDir = File(tempDirForApiGeneratorOutputs.toFile(), "java-compiled")
 
             URLClassLoader(apiGeneratorUrls).use {
@@ -127,23 +124,21 @@ abstract class ExtractSdkShimTransform : TransformAction<ExtractSdkShimTransform
                  stream.filter { Files.isRegularFile(it) }.collect(Collectors.toList())
             }
 
+            compileKotlin(generatedFiles, totalClasspathStr, kotlinOutJar)
             // Java sources are produced by the apigenerator invoking the aidl compiler, therefore
             // java sources do not reference Kotlin sources.
-            compileKotlin(generatedFiles, totalClasspathStr, kotlinOutDir)
             compileJava(generatedFiles, totalClasspathStr, javaOutDir)
 
+            // As the Kotlin compiler may interact with files out of process, we produce a
+            // compiled jar to prevent accessing files that may still be accessed by other processes,
+            // we then add the compiled java to the jar for the transform output jar.
             ZipOutputStream(BufferedOutputStream(output.outputStream())).use { outJar ->
-                writeCompiledClassesToZip(kotlinOutDir, outJar)
+                writeCompiledClassesToZip(kotlinOutJar, outJar)
                 writeCompiledClassesToZip(javaOutDir, outJar)
             }
         }
         finally {
-            try {
-                FileUtils.deleteRecursivelyIfExists(tempDirForApiGeneratorOutputs.toFile())
-            } catch (e: AccessDeniedException) {
-                Logger.getLogger(ExtractCompileSdkShimTransform::class.java.name)
-                        .log(Level.WARNING, e.message)
-            }
+            FileUtils.deleteRecursivelyIfExists(tempDirForApiGeneratorOutputs.toFile())
         }
     }
 
@@ -169,7 +164,8 @@ abstract class ExtractSdkShimTransform : TransformAction<ExtractSdkShimTransform
         ).call()
     }
 
-    private fun compileKotlin(generatedFiles: List<Path>, totalClasspathStr: String, kotlinOutDir: File) {
+    private fun compileKotlin(
+            generatedFiles: List<Path>, totalClasspathStr: String, kotlinOutJar: File) {
         execOperations.javaexec { spec ->
             spec.mainClass.set("org.jetbrains.kotlin.cli.jvm.K2JVMCompilerKt")
             spec.classpath(parameters.kotlinCompiler)
@@ -177,7 +173,7 @@ abstract class ExtractSdkShimTransform : TransformAction<ExtractSdkShimTransform
                     "-no-jdk",
                     "-no-reflect") + generatedFiles.map { it.pathString } + listOf(
                     "-classpath", totalClasspathStr,
-                    "-d", kotlinOutDir.path)
+                    "-d", kotlinOutJar.path)
         }
     }
 
