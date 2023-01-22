65bdd138d54715dba400869e733f8e3b52af8379
bingran <bingran@google.com>
2023-01-21 17:35:26 -0800

    Upgrade gradle version to 8.0-rc-2
    Bug: 265809620
    Test: existing
    Change-Id: I22900ba82bcb6cf7a46c4463fb2421f0fd35b168

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -2787,7 +2787,7 @@ public final class SdkConstants {
      * Studio product. It need not actually be the latest version of Gradle, but it will most likely
      * be fairly recent.
      */
-    public static final String GRADLE_LATEST_VERSION = "8.0-rc-1";
+    public static final String GRADLE_LATEST_VERSION = "8.0-rc-2";
 
     /**
      * The minimum released version of the Android Gradle Plugin that this version of Studio will
