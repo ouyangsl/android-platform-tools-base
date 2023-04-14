a5233950c07f87b6427360dfdba6fd60cdc58065
Eugene Yakavets <yakavets@google.com>
2023-04-14 19:21:37 +0000

    Use remote execution proxy for building linux fsNotifier
    Bug: 277257229
    Change-Id: I9520d9f2ae386272354875c75a74f41c13a9311f

--- intellij-native/linux_tools.sh
+++ intellij-native/linux_tools.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "ENV"
-env
-echo "PWD $PWD"
-echo "ls -lha"
-ls -lha
+# Creates the directory if it does not exist and returns its absolute path
+function make_target_dir() {
+  mkdir -p "$1" && cd "$1" && pwd
+}
 
-echo "Done!"
+declare -r top=$(pwd)
+declare -r out_dir=$(make_target_dir "$1")
+declare -r dist_dir=$(make_target_dir "$2")
+declare -r build_number="$3"
+
+# Build fsNotifier
+(
+    mkdir $out_dir/fsNotifier && cd $out_dir/fsNotifier
+    cp -r $top/tools/idea/native/fsNotifier/linux/. ./
+    ./make.sh
+    cp fsnotifier $dist_dir/.
+)
+
+echo "Done Building IntelliJ Linux Tools!"
