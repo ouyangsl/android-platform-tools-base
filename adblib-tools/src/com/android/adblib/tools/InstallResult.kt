a26b991781fb3acfb380fcb851c344c8d91cf220
Alexander Demurov <ademurov@google.com>
2023-04-21 14:53:18 -0700

    Remove unused values and fix nullability for a parameter
    Also fixes some other warnings.
    
    Bug: n/a
    Test: n/a
    Change-Id: I7f18b3bcb93170ae4eb3d0b0e4ebf8c299d1e9fe

--- adblib-tools/src/com/android/adblib/tools/InstallResult.kt
+++ adblib-tools/src/com/android/adblib/tools/InstallResult.kt
@@ -22,7 +22,7 @@ import java.util.regex.Pattern
 // "Success: streamed 13740091 bytes"
 // A typical success output from "install-commit" is:
 // "Success"
-internal val SUCCESS_OUTPUT = "Success"
+internal const val SUCCESS_OUTPUT = "Success"
 
 // Parse install-commit output
 // Example of failure message:
@@ -33,46 +33,42 @@ internal class InstallResult(result: String) {
 
     val errorMessage : String
     val errorCode : String
-    val successMessage : String
     val success : Boolean
 
     init {
         var tmpErrorMessage = ""
         var tmpErrorCode = ""
-        var tmpSuccessMessage = ""
         var tmpSuccess = true
         // TODO: We should have a flow returning lines instead of breaking the output down
-            val lines = result.split("\n")
+        val lines = result.split("\n")
 
-            lines.forEach {
-                if (it.isEmpty()) {
-                    return@forEach
-                }
-
-                if (it.startsWith(SUCCESS_OUTPUT)) {
-                    tmpSuccess = true
-                    tmpErrorMessage = ""
-                    tmpSuccessMessage = it
-                    return@forEach
-                }
-
-                tmpSuccess = false
-                val m: Matcher = FAILURE_PATTERN.matcher(it)
-                if (m.matches()) {
-                    tmpErrorMessage = m.group(1) // e.g.: INSTALL_FAILED_TEST_ONLY: installPackageLI
-                    tmpErrorCode = m.group(2) // e.g.: INSTALL_FAILED_TEST_ONLY
-                } else {
-                    if (tmpErrorMessage.isEmpty()) {
-                        tmpErrorMessage = "Unknown failure: '$it'"
-                        tmpErrorCode = "UNKNOWN"
-                    } else {
-                        tmpErrorMessage += "\n $it"
-                   }
-               }
+        lines.forEach {
+            if (it.isEmpty()) {
+                return@forEach
             }
+
+            if (it.startsWith(SUCCESS_OUTPUT)) {
+                tmpSuccess = true
+                tmpErrorMessage = ""
+                return@forEach
+            }
+
+            tmpSuccess = false
+            val m: Matcher = FAILURE_PATTERN.matcher(it)
+            if (m.matches()) {
+                tmpErrorMessage = m.group(1) // e.g.: INSTALL_FAILED_TEST_ONLY: installPackageLI
+                tmpErrorCode = m.group(2) // e.g.: INSTALL_FAILED_TEST_ONLY
+            } else {
+                if (tmpErrorMessage.isEmpty()) {
+                    tmpErrorMessage = "Unknown failure: '$it'"
+                    tmpErrorCode = "UNKNOWN"
+                } else {
+                    tmpErrorMessage += "\n $it"
+                }
+            }
+        }
         errorMessage = tmpErrorMessage
         errorCode = tmpErrorCode
-        successMessage = tmpSuccessMessage
         success = tmpSuccess
     }
 }
