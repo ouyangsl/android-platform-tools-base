8ddee54511f4f2bc691b5fc6cc870d07a771fdee
Phil Nguyen <philnguyen@google.com>
2023-05-03 23:41:37 +0000

    Each environment only takes its own specific configuration type
    Bug: n/a
    Test: existing
    Change-Id: I06dea480ebd7a150263d406e8a3518f75ab5a7cc

--- lint/cli/src/main/java/com/android/tools/lint/FirUastEnvironment.kt
+++ lint/cli/src/main/java/com/android/tools/lint/FirUastEnvironment.kt
@@ -72,7 +72,7 @@ private constructor(
 
   companion object {
     @JvmStatic
-    fun create(config: UastEnvironment.Configuration): FirUastEnvironment {
+    fun create(config: Configuration): FirUastEnvironment {
       val parentDisposable = Disposer.newDisposable("FirUastEnvironment.create")
       val analysisSession = createAnalysisSession(parentDisposable, config)
       return FirUastEnvironment(
@@ -97,7 +97,7 @@ private fun createKotlinCompilerConfig(enableKotlinScripting: Boolean): Compiler
 
 private fun createAnalysisSession(
   parentDisposable: Disposable,
-  config: UastEnvironment.Configuration
+  config: FirUastEnvironment.Configuration
 ): StandaloneAnalysisAPISession {
   // [configureApplicationEnvironment] will register app disposable and dispose it at
   // [UastEnvironment#disposeApplicationEnvironment].
