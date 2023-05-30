fd9f7606ab1446b5161f5062a690dbc312b61f67
Diego Perez <diegoperez@google.com>
2023-05-30 10:39:51 +0000

    Remove direct reference to IDEA module
    so we do not break the RenderModelModule abstraction.
    
    Bug: N/A
    Test: N/A
    Change-Id: I372e4985783b77ad0d4e812bd4916f0580bbf930

--- previewlib/cli/src/main/java/com/android/screenshot/cli/ScreenshotRenderModelModule.kt
+++ previewlib/cli/src/main/java/com/android/screenshot/cli/ScreenshotRenderModelModule.kt
@@ -26,6 +26,8 @@ import com.android.tools.idea.res.ScreenshotResourceRepositoryManager
 import com.android.tools.lint.model.LintModelModule
 import com.android.tools.module.AndroidModuleInfo
 import com.android.tools.module.ModuleDependencies
+import com.android.tools.rendering.ModuleKey
+import com.android.tools.rendering.ModuleKeyManager
 import com.android.tools.rendering.api.EnvironmentContext
 import com.android.tools.rendering.api.RenderModelManifest
 import com.android.tools.rendering.api.RenderModelModule
@@ -78,6 +80,8 @@ class ScreenshotRenderModelModule(
         }
     override val resourceIdManager: ResourceIdManager
         get() = resourceManager
+    override val moduleKey: ModuleKey
+        get() = ModuleKeyManager.getKey(composeModule.module)
     override val resourcePackage: String?
         get() = composeModule.module.getModuleSystem().getPackageName()
 
