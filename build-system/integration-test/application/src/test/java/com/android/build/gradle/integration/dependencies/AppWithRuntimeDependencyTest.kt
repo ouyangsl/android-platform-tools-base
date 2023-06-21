012b4c27a9e877ce09320b53212e82d58c6dee61
Ahmet Kerim Senol <akerim@google.com>
2023-06-21 09:41:53 +0000

    Convert AppWithRuntimeDependencyTest to Kotlin
    Bug: 278674114
    Test: self
    Change-Id: Iadb6ee8f356314ffef5f02db97e7995e4b9dbf13

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/AppWithRuntimeDependencyTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/AppWithRuntimeDependencyTest.kt
@@ -13,127 +13,104 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.build.gradle.integration.dependencies
 
-package com.android.build.gradle.integration.dependencies;
-
-import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat;
-import static com.android.build.gradle.integration.common.utils.TestFileUtils.appendToFile;
-
-import com.android.build.gradle.integration.common.fixture.GradleTestProject;
-import com.android.build.gradle.integration.common.utils.AndroidProjectUtils;
-import com.android.builder.model.AndroidLibrary;
-import com.android.builder.model.AndroidProject;
-import com.android.builder.model.Dependencies;
-import com.android.builder.model.Variant;
-import com.android.testutils.MavenRepoGenerator;
-import com.android.testutils.TestAarGenerator;
-import com.android.testutils.TestInputsGenerator;
-import com.android.utils.PathUtils;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
+import com.android.build.gradle.integration.common.fixture.GradleTestProject
+import com.android.build.gradle.integration.common.truth.TruthHelper
+import com.android.build.gradle.integration.common.utils.TestFileUtils
+import com.android.build.gradle.integration.common.utils.getVariantByName
+import com.android.builder.model.AndroidProject
+import com.android.testutils.MavenRepoGenerator
+import com.android.testutils.TestInputsGenerator
+import com.android.testutils.generateAarWithContent
+import com.android.utils.PathUtils
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
+import com.google.common.collect.Iterables
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.File
+import java.util.stream.Collectors
 
 /**
  * Test for runtime only dependencies. Test project structure: app -> library (implementation) ----
  * library -> library2 (implementation) ---- library -> [guava (implementation) and example aar]
  *
- * <p>The test verifies that the dependency model of app module contains library2, guava and the
+ *
+ * The test verifies that the dependency model of app module contains library2, guava and the
  * example aar as runtime only dependencies.
  */
-public class AppWithRuntimeDependencyTest {
+class AppWithRuntimeDependencyTest {
 
-    private static MavenRepoGenerator mavenRepo;
+    private val aar = generateAarWithContent(
+            "com.example.aar",
+            TestInputsGenerator.jarWithEmptyClasses(ImmutableList.of("com/example/MyClass")),
+            ImmutableMap.of())
+    private val mavenRepo =
+            MavenRepoGenerator(listOf(
+                    MavenRepoGenerator.Library(
+                            "com.example:aar:1",
+                            "aar",
+                            aar)))
+    @get:Rule val project = GradleTestProject.builder()
+            .fromTestProject("projectWithModules")
+            .withAdditionalMavenRepo(mavenRepo)
+            .create()
 
-    static {
-        try {
-            byte[] aar =
-                    TestAarGenerator.generateAarWithContent(
-                            "com.example.aar",
-                            TestInputsGenerator.jarWithEmptyClasses(
-                                    ImmutableList.of("com/example/MyClass")),
-                            ImmutableMap.of());
-            mavenRepo =
-                    new MavenRepoGenerator(
-                            ImmutableList.of(
-                                    new MavenRepoGenerator.Library(
-                                            "com.example:aar:1", "aar", aar)));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+
+    @Before
+    fun setUp() {
+        project.setIncludedProjects("app", "library", "library2")
+        TestFileUtils.appendToFile(
+                project.getSubproject("app").buildFile,
+                """
+                            dependencies {
+                                implementation project(':library')
+                            }
+                            """)
+        TestFileUtils.appendToFile(
+                project.getSubproject("library").buildFile,
+                """
+                            dependencies {
+                                implementation project(':library2')
+                                implementation 'com.google.guava:guava:19.0'
+                                implementation 'com.example:aar:1'
+                            }
+                            """)
     }
 
-    @ClassRule
-    public static GradleTestProject project =
-            GradleTestProject.builder()
-                    .fromTestProject("projectWithModules")
-                    .withAdditionalMavenRepo(mavenRepo)
-                    .create();
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        project.setIncludedProjects("app", "library", "library2");
-
-        appendToFile(
-                project.getSubproject("app").getBuildFile(),
-                "\ndependencies {\n" + "    implementation project(':library')\n" + "}\n");
-
-        appendToFile(
-                project.getSubproject("library").getBuildFile(),
-                "\ndependencies {\n"
-                        + "    implementation project(':library2')\n"
-                        + "    implementation 'com.google.guava:guava:19.0'\n"
-                        + "    implementation 'com.example:aar:1'\n"
-                        + "}\n");
-    }
-
-    @AfterClass
-    public static void cleanUp() {
-        project = null;
-    }
 
     @Test
-    public void checkRuntimeClasspathWithLevel1Model() throws Exception {
-        Map<String, AndroidProject> models =
-                project.model()
-                        .level(AndroidProject.MODEL_LEVEL_3_VARIANT_OUTPUT_POST_BUILD)
-                        .fetchAndroidProjects()
-                        .getOnlyModelMap();
-
-        Variant appDebug = AndroidProjectUtils.getVariantByName(models.get(":app"), "debug");
-
-        Dependencies deps = appDebug.getMainArtifact().getDependencies();
+    fun checkRuntimeClasspathWithLevel1Model() {
+        val models = project.model()
+                .level(AndroidProject.MODEL_LEVEL_3_VARIANT_OUTPUT_POST_BUILD)
+                .fetchAndroidProjects()
+                .onlyModelMap
+        val appDebug = models[":app"]!!.getVariantByName("debug")
+        val deps = appDebug.mainArtifact.dependencies
 
         // Verify that app has one AndroidLibrary dependency, :library.
-        Collection<AndroidLibrary> libs = deps.getLibraries();
-        assertThat(libs).named("app android library deps count").hasSize(1);
-        assertThat(Iterables.getOnlyElement(libs).getProject())
+        val libs = deps.libraries
+        TruthHelper.assertThat(libs).named("app android library deps count").hasSize(1)
+        TruthHelper.assertThat(Iterables.getOnlyElement(libs).project)
                 .named("app android library deps path")
-                .isEqualTo(":library");
+                .isEqualTo(":library")
 
         // Verify that app doesn't have module dependency.
-        assertThat(deps.getJavaModules()).named("app module dependency count").isEmpty();
+        TruthHelper.assertThat(deps.javaModules).named("app module dependency count").isEmpty()
 
         // Verify that app doesn't have JavaLibrary dependency.
-        assertThat(deps.getJavaLibraries()).named("app java dependency count").isEmpty();
+        TruthHelper.assertThat(deps.javaLibraries).named("app java dependency count").isEmpty()
 
         // Verify that app has runtime only dependencies on guava and the aar.
-        List<String> runtimeOnlyClasses =
-                deps.getRuntimeOnlyClasses()
-                        .stream()
-                        .map(file -> PathUtils.toSystemIndependentPath(file.toPath()))
-                        .collect(Collectors.toList());
-
-        assertThat(runtimeOnlyClasses).hasSize(2);
+        val runtimeOnlyClasses = deps.runtimeOnlyClasses
+                .stream()
+                .map { file: File -> PathUtils.toSystemIndependentPath(file.toPath()) }
+                .collect(Collectors.toList())
+        TruthHelper.assertThat(runtimeOnlyClasses).hasSize(2)
         // Verify the order of the artifacts too.
-        assertThat(runtimeOnlyClasses.get(0)).endsWith("/guava-19.0.jar");
-        assertThat(runtimeOnlyClasses.get(1)).endsWith("/aar-1/jars/classes.jar");
+        TruthHelper.assertThat(runtimeOnlyClasses[0]).endsWith("/guava-19.0.jar")
+        TruthHelper.assertThat(runtimeOnlyClasses[1]).endsWith("/aar-1/jars/classes.jar")
     }
 }
