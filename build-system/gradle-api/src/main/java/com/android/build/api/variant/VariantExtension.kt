ac5bf33f0ee515f93da4628f17a001d86c765db7
Jerome Dochez <jedo@google.com>
2023-06-21 22:56:00 -0700

    enhance javadoc to give more directions to users.
    Test: just changed a javadoc
    Bug: N/A
    Change-Id: Ibb50b5dd5d9d3eebc7cba131dd4947e4d81f1007

--- build-system/gradle-api/src/main/java/com/android/build/api/variant/VariantExtension.kt
+++ build-system/gradle-api/src/main/java/com/android/build/api/variant/VariantExtension.kt
@@ -25,5 +25,40 @@ package com.android.build.api.variant
  *
  * Since this type will most likely be used as [org.gradle.api.Task]'s input, your subtype
  * should also extend [java.io.Serializable]
+ *
+ * This variant extension will be passed to all plugins that register a
+ * [AndroidComponentsExtension.onVariants] block. In case several plugins modify some of the
+ * variant extension fields, it is primordial to define all the fields as a
+ * [org.gradle.api.provider.Property].  This will ensure that the tasks execution blocks that call
+ * [org.gradle.api.provider.Property.get] will get the final value, regardless of the order in which
+ * plugins are configured.
+ *
+ * Example of a sub-type :
+ *
+ * <code>
+ * abstract class VariantDslExtension @Inject constructor(
+ *     // Do not keep a reference on the VariantExtensionConfig as it is not serializable,
+ *     // use the constructor to extract the values for the BuildType/ProductFlavor extensions
+ *     // that can be obtained from the VariantExtensionConfig.
+ *     extensionConfig: VariantExtensionConfig<*>
+ * ): VariantExtension, java.io.Serializable {
+ *     abstract val variantSettingOne: Property<String>
+ *     abstract val variantSettingTwo: Property<Int>
+ * </code>
+ *
+ * and this is the corresponding registration and instantiation code :
+ * <code>
+ *    androidComponents.registerExtension(
+ *      DslExtension.Builder("dslExtension")
+ *          .extendProjectWith(ProjectDslExtension::class.java)
+ *          .build()
+ *    ) { config ->
+ *          project.objects.newInstance(
+ *              VariantDslExtension::class.java,
+ *              config
+ *          )
+ *    }
+ * </code>
+ *
  */
 interface VariantExtension
