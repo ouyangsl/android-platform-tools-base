cccbbf7df23ef5ff55efcc3b38ef29b89d08417b
Divya K <divyakk@google.com>
2023-08-30 21:01:19 +0000

    Create new PreviewScreenshotRenderTask and move cli invocation out of validation/update tasks
    Bug: 298250439
    Test: n/a
    Change-Id: I13c40f762156719c3276e7550df11dc23c791aad

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/PreviewScreenshotValidationTask.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/PreviewScreenshotValidationTask.kt
@@ -17,36 +17,22 @@
 package com.android.build.gradle.internal.tasks
 
 import com.android.Version
-import com.android.build.api.artifact.ScopedArtifact
-import com.android.build.api.variant.ScopedArtifacts
-import com.android.build.gradle.internal.SdkComponentsBuildService
 import com.android.build.gradle.internal.component.AndroidTestCreationConfig
 import com.android.build.gradle.internal.component.InstrumentedTestCreationConfig
-import com.android.build.gradle.internal.services.getBuildService
 import com.android.build.gradle.internal.tasks.factory.VariantTaskCreationAction
 import com.android.buildanalyzer.common.TaskCategory
 import com.android.builder.core.ComponentType
-import com.android.utils.usLocaleCapitalize
-import org.gradle.api.GradleException
 import org.gradle.api.Project
-import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.FileCollection
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 import org.gradle.api.tasks.VerificationTask
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.tasks.Classpath
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.jvm.toolchain.JavaToolchainService
 
 /**
  * Runs screenshot tests of a variant.
@@ -63,100 +49,21 @@ abstract class PreviewScreenshotValidationTask : NonIncrementalTask(), Verificat
     @Internal
     override lateinit var variantName: String
 
-    @get:InputFiles
-    @get:PathSensitive(PathSensitivity.NONE)
-    abstract val screenshotCliJar: ConfigurableFileCollection
-
-    @get:InputFiles
-    @get:PathSensitive(PathSensitivity.NONE)
-    abstract val sourceFiles: ConfigurableFileCollection
-
-    @get:InputDirectory
-    @get:PathSensitive(PathSensitivity.NONE)
-    abstract val lintModelDir: DirectoryProperty
-
-    @get:InputDirectory
-    @get:PathSensitive(PathSensitivity.NONE)
-    abstract val lintCacheDir: DirectoryProperty
-
     @get:InputDirectory
     @get:PathSensitive(PathSensitivity.NONE)
     abstract val goldenImageDir: DirectoryProperty
 
-    @get:OutputDirectory
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.NONE)
     abstract val imageOutputDir: DirectoryProperty
 
-    @get:Internal
-    abstract val layoutlibDir: ConfigurableFileCollection
-
-    @get:Classpath
-    abstract val testClassesDir: ConfigurableFileCollection
-
-    private val cliParams: MutableMap<String, String> = mutableMapOf()
-
     override fun doTaskAction() {
-        if (goldenImageDir.asFileTree.isEmpty) {
-            throw GradleException("No screenshots were found. To generate screenshots to"
-                    + " test against, run 'previewScreenshotUpdate${variantName.usLocaleCapitalize()}AndroidTest`")
-        }
-
-        cliParams["previewJar"] = screenshotCliJar.singleFile.absolutePath
-        cliParams["layoutlib.dir"] = layoutlibDir.singleFile.toPath().toString()
-        val testClassesDependencies = testClassesDir.files
-            .filter { it.exists() && it.isDirectory}.map { it.absolutePath + '/' }.joinToString(";")
-
-        // invoke CLI tool
-        val process = ProcessBuilder(
-            mutableListOf(cliParams["java"],
-                    "-cp", cliParams["previewJar"], "com.android.screenshot.cli.Main",
-                    "--client-name", cliParams["client.name"],
-                    "--client-version", cliParams["client.version"],
-                    "--jdk-home", cliParams["java.home"],
-                    "--sdk-home", cliParams["androidsdk"],
-                    "--lint-model", cliParams["lint.model"],
-                    "--cache-dir", cliParams["lint.cache"],
-                    "--root-lint-model", cliParams["lint.model"],
-                    "--output-location", cliParams["output.location"] + "/",
-                    "--golden-location", cliParams["golden.location"] + "/",
-                    "--file-path", cliParams["sources"]!!.split(",").first(),
-                    "--layoutlib-dir", cliParams["layoutlib.dir"],
-                    "--additional-deps", listOf(cliParams["additional.deps"]!!, testClassesDependencies).joinToString(File.pathSeparator))
-                ).apply {
-            environment().remove("TEST_WORKSPACE")
-            redirectErrorStream(true)
-            redirectOutput(ProcessBuilder.Redirect.INHERIT)
-        }.start()
-        process.waitFor()
-
-        var success: Boolean
-        var message = ""
-        when (process.exitValue()) {
-            in listOf(0, 3) -> {
-                success = true
-            }
-            in listOf(1, 2) -> {
-                success = false
-                message = "Tests failed. See report for details. "
-            }
-            else ->  {
-                success = false
-                message = "Unknown error code ${process.exitValue()} returned"
-            }
-        }
-
-        if (!success) {
-            throw GradleException(message)
-        }
     }
 
     class CreationAction(
             private val androidTestCreationConfig: AndroidTestCreationConfig,
             private val imageOutputDir: File,
             private val goldenImageDir: File,
-            private val layoutlibDir: FileCollection,
-            private val lintModelDir: File,
-            private val lintCacheDir: File,
-            private val additionalDependencyPaths: List<String>
     ) :
             VariantTaskCreationAction<
                     PreviewScreenshotValidationTask,
@@ -175,70 +82,13 @@ abstract class PreviewScreenshotValidationTask : NonIncrementalTask(), Verificat
 
             task.group = JavaBasePlugin.VERIFICATION_GROUP
 
-            creationConfig.sources.kotlin?.getVariantSources()?.forEach {
-                task.sourceFiles.from(
-                        it.asFileTree { task.project.objects.fileTree() }
-                )
-            }
-            task.sourceFiles.disallowChanges()
-            task.cliParams["sources"] =
-                    task.sourceFiles.files.map { it.absolutePath }.joinToString(",")
-
             maybeCreatePreviewlibCliToolConfiguration(task.project)
-            task.screenshotCliJar.from(
-                    task.project.configurations.getByName(previewlibCliToolConfigurationName)
-            )
-
-            val toolchain = task.project.extensions.getByType(JavaPluginExtension::class.java).toolchain
-            val service = task.project.extensions.getByType(JavaToolchainService::class.java)
-            // TODO(b/295886078) Investigate error handling needed for getting JavaLauncher.
-            val javaLauncher = try {
-                service.launcherFor(toolchain).get()
-            } catch (ex: GradleException) {
-                // If the JDK that was set is not available get the JDK 11 as a default
-                service.launcherFor { toolchainSpec ->
-                    toolchainSpec.languageVersion.set(JavaLanguageVersion.of(11))
-                }.get()
-            }
-            task.cliParams["java"] =
-                    javaLauncher.executablePath.asFile.absolutePath
-            task.cliParams["java.home"] =
-                    javaLauncher.metadata.installationPath.asFile.absolutePath
-
-            task.cliParams["androidsdk"] =
-                    getBuildService(
-                            creationConfig.services.buildServiceRegistry,
-                            SdkComponentsBuildService::class.java)
-                            .get().sdkDirectoryProvider.get().asFile.absolutePath
 
             task.goldenImageDir.set(goldenImageDir)
             task.goldenImageDir.disallowChanges()
-            task.cliParams["golden.location"] = goldenImageDir.absolutePath
 
             task.imageOutputDir.set(imageOutputDir)
             task.imageOutputDir.disallowChanges()
-            task.cliParams["output.location"] = imageOutputDir.absolutePath
-
-            task.lintModelDir.set(lintModelDir)
-            task.lintModelDir.disallowChanges()
-            task.cliParams["lint.model"] = lintModelDir.absolutePath
-
-            task.lintCacheDir.set(lintCacheDir)
-            task.lintCacheDir.disallowChanges()
-            task.cliParams["lint.cache"] = lintCacheDir.absolutePath
-
-            task.cliParams["client.name"] = "Android Gradle Plugin"
-            task.cliParams["client.version"] = Version.ANDROID_GRADLE_PLUGIN_VERSION
-
-            task.testClassesDir.from(creationConfig.services.fileCollection().apply {from(creationConfig
-                .artifacts.forScope(ScopedArtifacts.Scope.PROJECT)
-                .getFinalArtifacts(ScopedArtifact.CLASSES)) })
-            task.testClassesDir.disallowChanges()
-
-            task.cliParams["additional.deps"] = additionalDependencyPaths.joinToString (";")
-
-            task.layoutlibDir.from(layoutlibDir)
-            task.layoutlibDir.disallowChanges()
 
         }
 
