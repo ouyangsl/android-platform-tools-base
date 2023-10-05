5aee18589f1f9a19280c2d46fd89720fb24d40a3
Paige Mcauliffe <paigemca@google.com>
2023-10-05 19:28:33 +0000

    Create directory and module for Screenshot plugin
    Bug: 303687369
    Test: built locally with bazel build //tools/base/preview/screenshot/preview-screenshot-gradle-plugin
    Change-Id: I19ed6dde4e11e45bef3c6c01395a393edd8d3b35

---
+++ preview/screenshot/preview-screenshot-gradle-plugin/src/main/java/com/android/tools/preview/screenshot/PreviewScreenshotGradlePlugin.kt
@@ -0,0 +1,12 @@
+package com.android.tools.preview.screenshot
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * An entry point for Screenshot plugin that adds support for screenshot testing on Compose Previews
+ */
+class PreviewScreenshotGradlePlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+    }
+}
