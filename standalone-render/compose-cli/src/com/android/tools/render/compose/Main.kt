7cbcb2f6131805025dc537c446b177d90ab2aeb0
Fedor <kudasov@google.com>
2023-10-13 14:28:21 +0100

    Remove redundant code from standlonane-render.cli
    Now that the JSON API was tested and prototyped in AGP.
    
    Bug: 265765901
    Test: N/A
    Change-Id: I767b3a5af40bf59104b39ddd116e7c4419ccdcc1

--- standalone-render/compose-cli/src/com/android/tools/render/compose/Main.kt
+++ standalone-render/compose-cli/src/com/android/tools/render/compose/Main.kt
@@ -19,40 +19,15 @@ package com.android.tools.render.compose
 import com.android.tools.preview.applyTo
 import com.android.tools.preview.resolve
 import com.android.tools.render.RenderRequest
-import com.android.tools.render.compose.agp.obtainPreviewsAndSerialize
-import com.android.tools.render.compose.rubish.RenderDependencies.Companion.readFromDump
 import com.android.tools.render.render
-import java.io.File
 
 fun main(args: Array<String>) {
-    // This is to be removed completely once AGP starts producing Json with the correct format
-    val settingsPath = if (args.isEmpty()) "rendering_settings.txt" else args[0]
-    val renderSettings = File(settingsPath).readLines().map { it.split(":") }.associate { it[0] to it[1] }
-    val layoutlibPath = renderSettings["layoutlib"]!!
-    val sdkPath = renderSettings["sdk"]!!
-    val outputFolder = renderSettings["output"]!!
+    if (args.isEmpty()) {
+        println("Path to the Compose rendering settings file is missing.")
+        return
+    }
 
-    val renderDeps = readFromDump(renderSettings["dependencies"]!!)
-
-    val classPath = renderDeps.classesJars + listOf(renderDeps.mainClasses)
-    val packageName = renderDeps.packageName
-    val resourceApkPath = renderDeps.resourcesApk
-
-    val inputJsonFile = "json.txt"
-
-    // This should be moved to the AGP
-    obtainPreviewsAndSerialize(
-        classPath,
-        sdkPath,
-        layoutlibPath,
-        outputFolder,
-        packageName,
-        resourceApkPath,
-        inputJsonFile
-    )
-
-    // This is the first thing we should actually do in the compose-cli
-    val composeRendering = readComposeRenderingJson(inputJsonFile)
+    val composeRendering = readComposeRenderingJson(args[0])
 
     val renderRequests = composeRendering.screenshots.mapNotNull { screenshot ->
         screenshot.toPreviewElement()?.let { previewElement ->
