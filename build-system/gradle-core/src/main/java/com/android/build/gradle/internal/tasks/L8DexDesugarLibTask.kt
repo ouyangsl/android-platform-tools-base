0678b0f481c8ac2c0b07ab832bfb794ffe7be28f
bingran <bingran@google.com>
2023-10-16 11:09:09 -0700

    Follow up: Don't retrieve feature dex in android test component
    This is a follow up CL to address comments in I903a6a9759fb5e4cf9cbc50032ab8fc7fec97c6a
    
    Bug: 304082728
    Test: CoreLibraryDesugarDynamicFeatureTest#minifyAndroidTestSmokeTest
    Change-Id: Ib745cf7ec46813b515a5626c47cc6c6a6ed24598

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/L8DexDesugarLibTask.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/tasks/L8DexDesugarLibTask.kt
@@ -182,7 +182,7 @@ abstract class L8DexDesugarLibTask : NonIncrementalTask() {
                 // For feature dex, it is produced by d8/r8 in feature module and published to
                 // application(not for androidTest component). The reason why we don't use
                 //  consumesFeatureJars is because that API is only for minified build.
-                if (creationConfig is ApplicationCreationConfig &&
+                if (!creationConfig.componentType.isForTesting &&
                     creationConfig.global.hasDynamicFeatures) {
                     task.dexFiles.from(
                         creationConfig.variantDependencies.getArtifactFileCollection(
