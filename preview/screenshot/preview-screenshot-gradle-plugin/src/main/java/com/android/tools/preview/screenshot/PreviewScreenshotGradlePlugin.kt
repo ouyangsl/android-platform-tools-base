37fb23ccf7fa265b9c2931d0c1a43b31cb6eb68b
Paige Mcauliffe <paigemca@google.com>
2023-10-17 16:16:09 +0000

    Check AGP version in PreviewScreenshotGradlePlugin
    Bug: 303687369
    Test: unit test added
    Change-Id: I3be8672804459284cca2ea0e393fc4357e41dd0d

--- preview/screenshot/preview-screenshot-gradle-plugin/src/main/java/com/android/tools/preview/screenshot/PreviewScreenshotGradlePlugin.kt
+++ preview/screenshot/preview-screenshot-gradle-plugin/src/main/java/com/android/tools/preview/screenshot/PreviewScreenshotGradlePlugin.kt
@@ -1,5 +1,8 @@
 package com.android.tools.preview.screenshot
 
+import com.android.build.api.AndroidPluginVersion
+import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.gradle.api.AndroidBasePlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,5 +11,17 @@ import org.gradle.api.Project
  */
 class PreviewScreenshotGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        project.plugins.withType(AndroidBasePlugin::class.java) {
+            val agpVersion =
+                    project.extensions.getByType(AndroidComponentsExtension::class.java).pluginVersion
+            if (agpVersion < AndroidPluginVersion(8, 3, 0).alpha(1)) {
+                error("Android Gradle plugin version 8.3.0-alpha01 or higher is required." +
+                        " Current version is $agpVersion.")
+            }
+            if (agpVersion >= AndroidPluginVersion(8,4).alpha(1)) {
+                error("Preview screenshot plugin is an experimental feature. It requires Android " +
+                        "Gradle plugin version 8.3.0. Current version is $agpVersion.")
+            }
+        }
     }
 }
