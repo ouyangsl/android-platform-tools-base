5221e359321022481098847a91be27f7a7754b7f
Hakan Mehmed <hmehmed@google.com>
2023-10-30 21:46:02 +0000

    Migrate multiple project tests to model v2
    Use golden files to check dependencies.
    
    Bug: 243119222
    Test: updating them
    Change-Id: Id29f6599e84b47b3f7fa3c3a9cb0b4e9ccb94e87

---
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/dependencies/AppWithJarDependOnLibTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.dependencies
+
+import com.android.build.gradle.integration.common.fixture.model.ModelComparator
+import com.android.build.gradle.integration.common.fixture.testprojects.PluginType
+import com.android.build.gradle.integration.common.fixture.testprojects.createGradleProject
+import com.android.build.gradle.integration.common.fixture.testprojects.prebuilts.setUpHelloWorld
+import com.android.builder.model.v2.ide.SyncIssue
+import org.junit.Rule
+import org.junit.Test
+
+class AppWithJarDependOnLibTest : ModelComparator() {
+
+    @get:Rule
+    val project = createGradleProject {
+        subProject(":app") {
+            plugins.add(PluginType.ANDROID_APP)
+            android {
+                setUpHelloWorld()
+            }
+            dependencies {
+                api(project(":jar"))
+            }
+        }
+        subProject(":library") {
+            plugins.add(PluginType.ANDROID_LIB)
+            android {
+                setUpHelloWorld()
+            }
+        }
+        subProject(":jar") {
+            plugins.add(PluginType.JAVA_LIBRARY)
+            dependencies {
+                api(project(":library"))
+            }
+        }
+    }
+
+    @Test
+    fun `test VariantDependencies model`() {
+        val result =
+            project.modelV2()
+                .ignoreSyncIssues(SyncIssue.SEVERITY_WARNING)
+                .fetchModels(variantName = "debug")
+
+        with(result).compareVariantDependencies(
+            projectAction = { getProject(":app") }, goldenFile = "app_VariantDependencies"
+        )
+    }
+}
