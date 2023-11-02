4b0e8931bebe6aef377e7f0079d60cf8cc9cb671
Kurt Dresner <kdresner@google.com>
2023-11-02 13:31:12 -0700

    Add non-null versions of collections functions
    This change adds three utility methods that I constantly find myself trying to use even though they don't exist:
    
    * associateWithNotNull
    * associateByNotNull
    * associateNotNull
    
    Bug: N/A
    Test: CollectionsUtilsTest.kt
    Change-Id: Ia88e770a2d49c6b8972ae1c03de3dc90ab601ec2

--- common/src/test/java/com/android/utils/CollectionsUtilsTest.kt
+++ common/src/test/java/com/android/utils/CollectionsUtilsTest.kt
@@ -22,13 +22,33 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class CollectionsUtilsTest {
-    @Test
-    fun mapValuesNotNull() {
-        val map = mapOf(1 to 2, 2 to 4, 3 to 6, 4 to 8)
-        // Explicitly typed here to make sure we get the right type back.
-        val result: Map<Int, Int> = map.mapValuesNotNull {
-            if (it.key % 2 == 0) it.value * 2 else null
-        }
-        assertThat(result).isEqualTo(mapOf(2 to 8, 4 to 16))
-    }
+  @Test
+  fun mapValuesNotNull() {
+    val map = mapOf(1 to 2, 2 to 4, 3 to 6, 4 to 8)
+    // Explicitly typed here to make sure we get the right type back.
+    val result: Map<Int, Int> = map.mapValuesNotNull { if (it.key % 2 == 0) it.value * 2 else null }
+    assertThat(result).isEqualTo(mapOf(2 to 8, 4 to 16))
+  }
+
+  @Test
+  fun associateWithNotNull() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.associateWithNotNull { it.takeIf { n -> n % 2 == 0 }?.let { n -> 2 * n } }
+    assertThat(result).containsExactly(2, 4, 4, 8)
+  }
+
+  @Test
+  fun associateByNotNull() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.associateByNotNull { it.takeIf { n -> n % 2 == 0 }?.let { n -> 2 * n } }
+    assertThat(result).containsExactly(4, 2, 8, 4)
+  }
+
+  @Test
+  fun associateNotNull() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result =
+      list.associateNotNull { it.takeIf { n -> n % 2 == 0 }?.let { n -> 2 * n to 3 * n } }
+    assertThat(result).containsExactly(4, 6, 8, 12)
+  }
 }
