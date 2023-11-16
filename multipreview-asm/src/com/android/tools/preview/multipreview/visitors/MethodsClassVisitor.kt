cabb100bf942f7ba8d65a22731c61435578675c4
Fedor <kudasov@google.com>
2023-11-16 14:10:11 +0000

    Split MultipreviewClassVisitor into two classes
    This makes processing a bit faster (1.18s vs 1.4s) because only
    relevant processing is performed.
    
    Bug: 311368889
    Test: MultipreviewTest
    Change-Id: I25cbba534e298a2027f72c60517a51771a562bd8

---
+++ multipreview-asm/src/com/android/tools/preview/multipreview/visitors/MethodsClassVisitor.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.preview.multipreview.visitors
+
+import com.android.tools.preview.multipreview.Graph
+import com.android.tools.preview.multipreview.MultipreviewSettings
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+
+/**
+ * [ClassVisitor] that looks for potential methods related to the multipreview. For every method, if
+ * not skipped, it delegates the job to the [AnnotatedMethodVisitor]
+ */
+internal class MethodsClassVisitor(
+    private val settings: MultipreviewSettings,
+    private val className: String,
+    private val graph: Graph,
+    private val methodsFilter: MethodsFilter,
+) : ClassVisitor(Opcodes.ASM8) {
+
+    override fun visitMethod(
+        access: Int,
+        name: String?,
+        descriptor: String?,
+        signature: String?,
+        exceptions: Array<out String>?,
+    ): MethodVisitor? {
+        return if (name == null || descriptor == null || !methodsFilter.allowMethod("$className.$name"))
+            super.visitMethod(access, name, descriptor, signature, exceptions)
+        else
+            AnnotatedMethodVisitor(settings, graph, "$className.$name")
+    }
+}
