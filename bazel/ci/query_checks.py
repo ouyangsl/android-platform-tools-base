8d1141e14ab54ffee1203dafbeef4a2fc258db90
Tom Renn <tomrenn@google.com>
2023-11-16 15:51:25 -0800

    Add query checks for ci:studio-mac tags
    When tagging tests for ci:studio-mac, they should have a cpu:N tag.
    Additionally, any tests depending on gradle should be using cpu:4 or
    greater.
    
    Bug: 311776406
    Test: Running locally
    Change-Id: Ice39b9a3818b7e006e07a1ef33755518240c167e

--- bazel/ci/query_checks.py
+++ bazel/ci/query_checks.py
@@ -44,4 +44,39 @@ def no_local_genrules(build_env: bazel.BuildEnv):
 
 def require_cpu_tags(build_env: bazel.BuildEnv):
   """Require certain targets have cpu:N tags."""
-  pass  # Implemented in follow-up change.
+  bazel_cmd = bazel.BazelCmd(build_env)
+  result = bazel_cmd.query(
+    'attr(tags, "ci:studio-mac", //...) except attr(tags, "cpu:[0-9]+", //...)')
+  if not result.stdout:
+    return
+
+  raise BuildGraphException(
+    title='MacOS test must have cpu:[0-9] tag',
+    go_link='go/studio-ci#macos',
+    body='ERROR: The following targets are missing a cpu:N tag.\n'+result.stdout
+  )
+
+
+def gradle_requires_cpu4_or_more(build_env: bazel.BuildEnv):
+  """Tests running on MacOS using Gradle must declare cpu:4 or higher."""
+  bazel_cmd = bazel.BazelCmd(build_env)
+  studio_mac_tags = 'attr(tags, "ci:studio-mac", //...)'
+  high_cpu_tags = 'attr(tags, "cpu:([4-9]|[1-9][1-9])", //...)'
+  paths_to_gradle = f'allpaths({studio_mac_tags} except {high_cpu_tags}, //tools/base/build-system:gradle-distrib)'
+
+  result = bazel_cmd.query(paths_to_gradle, '--output=minrank')
+  if not result.stdout:
+    return
+  query_targets = result.stdout.decode('utf8').splitlines()
+  # --output=minrank will prefix each target with a number, where 0 represents
+  # root targets.
+  query_targets = [s.removeprefix('0') for s in query_targets if s.startswith('0')]
+  msg = 'ERROR: The following targets depend on //tools/base/build-system:gradle-distrib and must have the tag cpu:4 or greater.'
+  raise BuildGraphException(
+        title='Gradle tests need cpu:4 or greater',
+        go_link='go/studio-ci#macos',
+        body=(
+            'ERROR: The follow targets depend on //tools/base/build-system:gradle-distrib'
+            ' and must have cpu:4 or greater\n'
+        ) + '\n'.join(query_targets)
+    )
