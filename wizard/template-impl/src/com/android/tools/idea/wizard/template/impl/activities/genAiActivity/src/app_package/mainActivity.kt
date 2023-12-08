6ce813920b4bdb506be4832d9876823a24db8fed
James Rose <rosej@google.com>
2023-12-08 13:37:59 -0800

    Make the Gemini output scrollable
    This requires removing the LazyColumn, which is not needed.
    Also, don't submit empty input.
    
    Fixes: 315347750
    Fixes: 315367034
    Test: manual for now
    Change-Id: Idac768ae9cedff2e2b0c9a83995703cc4d63c96e

--- wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/genAiActivity/src/app_package/mainActivity.kt
+++ wizard/template-impl/src/com/android/tools/idea/wizard/template/impl/activities/genAiActivity/src/app_package/mainActivity.kt
@@ -32,8 +32,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.CircularProgressIndicator
@@ -100,7 +100,9 @@ fun SummarizeScreen(
 ) {
     var prompt by remember { mutableStateOf("") }
     Column(
-        modifier = Modifier.padding(all = 8.dp)
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Row {
             TextField(
@@ -112,7 +114,11 @@ fun SummarizeScreen(
                     .weight(8f)
             )
             TextButton(
-                onClick = { onSummarizeClicked(prompt) },
+                onClick = {
+                    if (prompt.isNotBlank()) {
+                        onSummarizeClicked(prompt)
+                    }
+                },
 
                 modifier = Modifier
                     .weight(2f)
@@ -139,19 +145,15 @@ fun SummarizeScreen(
             }
 
             is SummarizeUiState.Success -> {
-                LazyColumn {
-                    items(listOf(uiState.outputText)) { item ->
-                        Row(modifier = Modifier.padding(all = 8.dp)) {
-                            Icon(
-                                Icons.Outlined.Person,
-                                contentDescription = "Person Icon"
-                            )
-                            Text(
-                                text = item,
-                                modifier = Modifier.padding(horizontal = 8.dp)
-                            )
-                        }
-                    }
+                Row(modifier = Modifier.padding(all = 8.dp)) {
+                    Icon(
+                        Icons.Outlined.Person,
+                        contentDescription = "Person Icon"
+                    )
+                    Text(
+                        text = uiState.outputText,
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    )
                 }
             }
 
