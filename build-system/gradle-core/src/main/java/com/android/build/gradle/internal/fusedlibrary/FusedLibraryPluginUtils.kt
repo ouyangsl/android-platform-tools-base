d8d6183e0ccb50407ed033b30984230f6a9b0048
Luke Edgar <lukeedgar@google.com>
2024-02-23 13:29:14 +0000

    Remove unnecessary Privacy Sandbox configurations
    includedApiUnmerged and includedRuntimeUnmerged are not necessary for
    Privacy Sandbox and are a carryover from the Fused Library plugin configurations.
    
    Some additional cleanup.
    
    Bug: n/a
    Test: PrivacySandboxSdkTest
    Change-Id: I241414629cbfe3c16259cf39ff677bd46fb21408

--- build-system/gradle-core/src/main/java/com/android/build/gradle/internal/fusedlibrary/FusedLibraryPluginUtils.kt
+++ build-system/gradle-core/src/main/java/com/android/build/gradle/internal/fusedlibrary/FusedLibraryPluginUtils.kt
@@ -20,7 +20,6 @@ import com.android.build.api.artifact.Artifact
 import com.android.build.api.artifact.impl.ArtifactsImpl
 import com.android.build.gradle.internal.DependencyConfigurator
 import com.android.build.gradle.internal.SdkComponentsBuildService
-import com.android.build.gradle.internal.component.ConsumableCreationConfig
 import com.android.build.gradle.internal.dependency.FilterShrinkerRulesTransform
 import com.android.build.gradle.internal.dependency.VersionedCodeShrinker
 import com.android.build.gradle.internal.publishing.AndroidArtifacts
@@ -34,8 +33,10 @@ import com.android.build.gradle.options.BooleanOption
 import com.android.builder.model.v2.ide.ProjectType
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.transform.TransformSpec
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition
+import org.gradle.api.attributes.Usage
 import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Provider
 
@@ -101,3 +102,37 @@ internal fun getDslServices(project: Project, projectServices: ProjectServices):
     return DslServicesImpl(projectServices, sdkComponentsBuildService, ProjectType.FUSED_LIBRARIES)
 }
 
+fun configureElements(
+        project: Project,
+        elements: Configuration,
+        usage: String,
+        artifacts: ArtifactsImpl,
+        publications: Map<Artifact.Single<RegularFile>, AndroidArtifacts.ArtifactType>,
+) {
+    elements.attributes.attribute(
+            Usage.USAGE_ATTRIBUTE,
+            project.objects.named(Usage::class.java, usage)
+    )
+    elements.isCanBeResolved = false
+    elements.isCanBeConsumed = true
+    elements.isTransitive = true
+
+    elements.outgoing.variants { variants ->
+        for (publication in publications) {
+            // we are only interested in the last provider in the chain of transformers for this bundle.
+            // Obviously, this is theoretical at this point since there is no variant API to replace
+            // artifacts, there is always only one.
+            val bundleTaskProvider = publication.key.let {
+                artifacts
+                        .getArtifactContainer(it)
+                        .getTaskProviders()
+                        .last()
+            }
+            variants.create(publication.value.type) { variant ->
+                variant.artifact(bundleTaskProvider) { artifact ->
+                    artifact.type = publication.value.type
+                }
+            }
+        }
+    }
+}
