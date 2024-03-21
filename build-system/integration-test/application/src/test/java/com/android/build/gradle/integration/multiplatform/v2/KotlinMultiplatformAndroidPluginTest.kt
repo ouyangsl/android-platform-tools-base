599a8ce876a93fa22a4b55421c2a56da9e663b46
Eric Reed <ericreed@google.com>
2024-03-21 16:35:36 -0700

    Disable flaky test KotlinMultiplatformAndroidPluginTest.testKmpLibraryTestApkContentsWithBuildTypeSelection
    Failure rate of 8%
    
    Bug: 329887373
    Test: N/A
    Change-Id: Ia9cd3bb8cda3392fa36933e0e70304f4f208df39

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/multiplatform/v2/KotlinMultiplatformAndroidPluginTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/multiplatform/v2/KotlinMultiplatformAndroidPluginTest.kt
@@ -29,6 +29,7 @@ import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
 import org.junit.Assume
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -59,6 +60,7 @@ class KotlinMultiplatformAndroidPluginTest(private val publishLibs: Boolean) {
         }
     }
 
+    @Ignore("b/329887373")
     @Test
     fun testKmpLibraryTestApkContentsWithBuildTypeSelection() {
         TestFileUtils.appendToFile(
