322e64783b6c7b76aff0064719ac232e5a8bbc26
Alon Albert <aalbert@google.com>
2024-04-12 09:10:04 -0700

    [Cleanup] Move Test Code Into testSrc
    Bug: NA
    Test: NA
    Change-Id: I5376aed2d4fcb5e7b1a57f0ca92dffaa5116b6b6

--- app-inspection/inspectors/common/testSrc/com/android/tools/appinspection/common/testing/FakeArtTooling.kt
+++ app-inspection/inspectors/common/testSrc/com/android/tools/appinspection/common/testing/FakeArtTooling.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.tools.appinspection.common
+package com.android.tools.appinspection.common.testing
 
 import androidx.inspection.ArtTooling
 
