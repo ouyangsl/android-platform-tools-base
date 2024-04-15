bbd4bcc35d35806110c9dd38a2b11ffaef3c83d1
Tor Norbye <tnorbye@google.com>
2024-04-15 17:37:13 +0100

    331666842: Allow add references only via method references
    Fixes: 331666842
    Test: Unit test included
    Change-Id: Ica42233cd0638a6215142f2004465745bebcfa63

--- lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/BuildListDetectorTest.kt
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/BuildListDetectorTest.kt
@@ -157,4 +157,116 @@ class BuildListDetectorTest : AbstractCheckTest() {
       .run()
       .expectClean()
   }
+
+  fun testMethodReferences() {
+    // Regression test for 331666842 comment 2
+    lint()
+      .files(
+        kotlin(
+            """
+            import android.database.Cursor
+
+            fun testOk1(): List<String> {
+              return buildList { // OK 1
+                val m: (String)->Boolean = ::add
+                m("test")
+              }
+            }
+
+            fun testOk2(): List<String> {
+              return buildList { // OK 2
+                val m: (String)->Boolean = this::add
+                m("test")
+              }
+            }
+
+            fun testWrongList(): List<String> {
+              val list = mutableListOf<String>()
+              return buildList { // ERROR: wrong list
+                val m: (String)->Boolean = list::add
+                m("test")
+              }
+            }
+
+            class PartData
+            private fun retrievePartData(): PartData? = nullo
+
+            private fun queryMmsPartTable(query: Cursor?) {
+                query.use { cursor ->
+                        buildList {
+                            while (cursor.moveToNext()) {
+                                retrievePartData()
+                                    ?.let(::add)
+                            }
+                        }
+                    }
+            }
+            """
+          )
+          .indented()
+      )
+      .run()
+      .expect(
+        """
+        src/PartData.kt:19: Warning: No add calls within buildList lambda; this is usually a mistake [BuildListAdds]
+          return buildList { // ERROR: wrong list
+                 ~~~~~~~~~
+        0 errors, 1 warnings
+        """
+      )
+  }
+
+  fun testScenarios() {
+    lint()
+      .files(
+        kotlin(
+            """
+            fun example(b: List<String>) {
+                buildList { add(" ") } // OK
+                buildList { addAll(listOf("", "")) } // OK
+                buildList { this.add(" ") } // OK
+                buildList { this.addAll(b) } // OK
+                buildList { someFunc(this, " ") } // OK
+                buildList { this.extensionFunc(" ") } // OK
+                buildList { extensionFunc(" ") } // OK
+                buildList { this += " " } // OK
+                buildList { plusAssign(" ") } // OK
+                buildList { this.plusAssign(" ") } // OK
+                buildList { listIterator().add(" ") } // OK
+                buildList { this.listIterator().add(" ") } // OK
+
+
+                buildList { val t = this; t.add(" ") } // OK
+                buildList { val t = this; t.addAll(listOf("", "")) } // OK
+                buildList { val t = this; t += " " } // OK
+                buildList { val t = this; t.extensionFunc(" ") } // OK
+                buildList { val t = this; t.plusAssign(" ") } // OK
+                buildList { val t = this; someFunc(t, " ") } // OK
+
+                buildList { this + this } // OK
+                buildList { this + " " } // OK
+                buildList { plus(" ") } // OK
+                buildList { returnsNewList(this) } // OK
+            }
+
+            private fun <E> MutableList<E>.extensionFunc(e: E) {
+                if (contains(e)) {
+                    add(e)
+                }
+            }
+
+            private fun <E> someFunc(l: MutableList<E>, e: E) {
+                if (l.contains(e)) {
+                    l.add(e)
+                }
+            }
+
+            private fun returnsNewList(l: MutableList<String>) = listOf(1, 2, 3) + l
+            """
+          )
+          .indented()
+      )
+      .run()
+      .expectClean()
+  }
 }
