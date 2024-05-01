8808367dc24549426f3729bd13eb1cef7e31e365
Scott Pollom <spollom@google.com>
2024-05-01 01:47:04 -0700

    Configure kotlin platform attribute for screenshotTest and testFixtures
    The same was done for all components when KGP was not applied
    (Ief92d9d46), but it's also required for screenshotTest and testFixtures
    components, even when KGP is applied.
    
    This change also splits the previous BuiltInKotlinSupportTest into two
    separate classes (one for screenshotTest and one for testFixtures)
    
    This change also increments the shard count for the
    built-in-kotlin-support-tests bazel target (b/337942969)
    
    Fixes: 338512598
    Bug: 337942969
    Test: BuiltInKotlinForScreenshotTestTest, etc.
    Change-Id: Iab3a27dbf511572dba88f37e9b97ab362917d291

--- build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/kotlin/BuiltInKotlinForScreenshotTestTest.kt
+++ build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/kotlin/BuiltInKotlinForScreenshotTestTest.kt
@@ -20,17 +20,15 @@ import com.android.build.gradle.integration.common.fixture.GradleTestProject.Com
 import com.android.build.gradle.integration.common.fixture.testprojects.PluginType
 import com.android.build.gradle.integration.common.fixture.testprojects.createGradleProjectBuilder
 import com.android.build.gradle.integration.common.fixture.testprojects.prebuilts.setUpHelloWorld
-import com.android.build.gradle.integration.common.truth.TruthHelper.assertThat
 import com.android.build.gradle.integration.common.utils.TestFileUtils
 import com.android.build.gradle.internal.dsl.ModulePropertyKey.BooleanWithDefault.SCREENSHOT_TEST
 import com.android.build.gradle.options.BooleanOption
 import com.android.testutils.TestUtils
-import com.android.testutils.apk.Aar
 import com.android.testutils.truth.PathSubject
 import org.junit.Rule
 import org.junit.Test
 
-class BuiltInKotlinSupportTest {
+class BuiltInKotlinForScreenshotTestTest {
 
     @get:Rule
     val project =
@@ -40,49 +38,41 @@ class BuiltInKotlinSupportTest {
                 plugins.add(PluginType.KOTLIN_ANDROID)
                 android {
                     setUpHelloWorld()
+                    minSdk = 21
+                }
+                appendToBuildFile {
+                    """
+                        kotlin {
+                            jvmToolchain(17)
+                        }
+
+                        """.trimIndent()
+                }
+            }
+            subProject(":lib2") {
+                plugins.add(PluginType.ANDROID_LIB)
+                plugins.add(PluginType.KOTLIN_ANDROID)
+                android {
+                    setUpHelloWorld()
+                }
+                appendToBuildFile {
+                    """
+                        kotlin {
+                            jvmToolchain(17)
+                        }
+
+                        """.trimIndent()
                 }
             }
         }.withKotlinGradlePlugin(true)
             .create()
 
+    /**
+     * Include dependency on "androidx.compose.ui:ui-tooling-preview:1.6.5" as a regression test for
+     * b/338512598
+     */
     @Test
-    fun testTestFixturesKotlinSupport() {
-        TestFileUtils.appendToFile(
-            project.gradlePropertiesFile,
-            "${BooleanOption.ENABLE_TEST_FIXTURES_KOTLIN_SUPPORT.propertyName}=true"
-        )
-        val lib = project.getSubproject(":lib")
-        lib.buildFile.appendText(
-            """
-                android.testFixtures.enable = true
-
-                kotlin {
-                    jvmToolchain(17)
-                }
-
-                dependencies {
-                    testFixturesImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
-                }
-                """.trimIndent()
-        )
-        lib.file("src/testFixtures/kotlin/LibTestFixtureFoo.kt").let {
-            it.parentFile.mkdirs()
-            it.writeText(
-                """
-                    package com.foo.library
-                    class LibTestFixtureFoo
-                    """.trimIndent()
-            )
-        }
-        lib.executor().run(":lib:assembleDebugTestFixtures")
-        val aar = lib.outputDir.resolve("aar").listFiles().single()
-        Aar(aar).use {
-            assertThat(it).containsMainClass("Lcom/foo/library/LibTestFixtureFoo;")
-        }
-    }
-
-    @Test
-    fun testScreenshotTest() {
+    fun testModuleAndExternalDependencies() {
         TestFileUtils.appendToFile(
             project.gradlePropertiesFile,
             "${BooleanOption.ENABLE_SCREENSHOT_TEST.propertyName}=true"
@@ -92,12 +82,10 @@ class BuiltInKotlinSupportTest {
             """
                 android.experimentalProperties["${SCREENSHOT_TEST.key}"] = true
 
-                kotlin {
-                    jvmToolchain(17)
-                }
-
                 dependencies {
-                    testFixturesImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
+                    screenshotTestImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
+                    screenshotTestImplementation("androidx.compose.ui:ui-tooling-preview:1.6.5")
+                    screenshotTestImplementation(project(":lib2"))
                 }
                 """.trimIndent()
         )
@@ -106,11 +94,27 @@ class BuiltInKotlinSupportTest {
             it.writeText(
                 """
                     package com.foo.library
+
+                    import com.foo.library.two.LibTwoClass
+                    import androidx.compose.ui.tooling.preview.Preview
+
                     class LibScreenshotTest
                     """.trimIndent()
             )
         }
-        lib.executor().run(":lib:compileDebugScreenshotTestKotlin")
+        val lib2 = project.getSubproject(":lib2")
+        lib2.file("src/main/kotlin/LibTwoClass.kt").let {
+            it.parentFile.mkdirs()
+            it.writeText(
+                """
+                    package com.foo.library.two
+                    class LibTwoClass
+                    """.trimIndent()
+            )
+        }
+        lib.executor()
+            .with(BooleanOption.USE_ANDROID_X, true)
+            .run(":lib:compileDebugScreenshotTestKotlin")
         val screenshotTestClassFile =
             project.getSubproject("lib")
                 .getIntermediateFile(
@@ -127,52 +131,7 @@ class BuiltInKotlinSupportTest {
     }
 
     @Test
-    fun testInternalModifierAccessibleFromTestFixtures() {
-        TestFileUtils.appendToFile(
-            project.gradlePropertiesFile,
-            "${BooleanOption.ENABLE_TEST_FIXTURES_KOTLIN_SUPPORT.propertyName}=true"
-        )
-        val lib = project.getSubproject(":lib")
-        lib.buildFile.appendText(
-            """
-                android.testFixtures.enable = true
-
-                kotlin {
-                    jvmToolchain(17)
-                }
-
-                dependencies {
-                    testFixturesImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
-                }
-                """.trimIndent()
-        )
-        lib.file("src/testFixtures/kotlin/LibTestFixtureFoo.kt").let {
-            it.parentFile.mkdirs()
-            it.writeText(
-                """
-                    package com.foo.library
-                    class LibTestFixtureFoo {
-                      init { LibFoo().bar() }
-                    }
-                    """.trimIndent()
-            )
-        }
-        lib.getMainSrcDir("java").resolve("LibFoo.kt").let {
-            it.parentFile.mkdirs()
-            it.writeText(
-                """
-                    package com.foo.library
-                    class LibFoo {
-                      internal fun bar() {}
-                    }
-                """.trimIndent()
-            )
-        }
-        lib.executor().run(":lib:assembleDebugTestFixtures")
-    }
-
-    @Test
-    fun testInternalModifierAccessibleFromScreenshotTest() {
+    fun testInternalModifierAccessible() {
         TestFileUtils.appendToFile(
             project.gradlePropertiesFile,
             "${BooleanOption.ENABLE_SCREENSHOT_TEST.propertyName}=true"
@@ -182,12 +141,8 @@ class BuiltInKotlinSupportTest {
             """
                 android.experimentalProperties["${SCREENSHOT_TEST.key}"] = true
 
-                kotlin {
-                    jvmToolchain(17)
-                }
-
                 dependencies {
-                    testFixturesImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
+                    screenshotTestImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
                 }
                 """.trimIndent()
         )
@@ -217,38 +172,7 @@ class BuiltInKotlinSupportTest {
     }
 
     @Test
-    fun testLowKotlinVersionWithTestFixturesKotlinSupport() {
-        TestFileUtils.searchAndReplace(
-            project.projectDir.parentFile.resolve(VERSION_CATALOG),
-            "version('kotlinVersion', '${TestUtils.KOTLIN_VERSION_FOR_TESTS}')",
-            "version('kotlinVersion', '1.8.10')"
-        )
-        TestFileUtils.appendToFile(
-            project.gradlePropertiesFile,
-            "${BooleanOption.ENABLE_TEST_FIXTURES_KOTLIN_SUPPORT.propertyName}=true"
-        )
-        val lib = project.getSubproject(":lib")
-        lib.buildFile.appendText(
-            """
-                android.testFixtures.enable = true
-
-                kotlin {
-                    jvmToolchain(17)
-                }
-
-                dependencies {
-                    testFixturesImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
-                }
-                """.trimIndent()
-        )
-        val result = lib.executor().expectFailure().run(":lib:assembleDebugTestFixtures")
-        result.assertErrorContains(
-            "The current Kotlin Gradle plugin version (1.8.10) is below the required"
-        )
-    }
-
-    @Test
-    fun testLowKotlinVersionWithScreenshotTest() {
+    fun testLowKotlinVersion() {
         TestFileUtils.searchAndReplace(
             project.projectDir.parentFile.resolve(VERSION_CATALOG),
             "version('kotlinVersion', '${TestUtils.KOTLIN_VERSION_FOR_TESTS}')",
@@ -263,12 +187,8 @@ class BuiltInKotlinSupportTest {
             """
                 android.experimentalProperties["${SCREENSHOT_TEST.key}"] = true
 
-                kotlin {
-                    jvmToolchain(17)
-                }
-
                 dependencies {
-                    testFixturesImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
+                    screenshotTestImplementation("org.jetbrains.kotlin:kotlin-stdlib:${TestUtils.KOTLIN_VERSION_FOR_TESTS}")
                 }
                 """.trimIndent()
         )
