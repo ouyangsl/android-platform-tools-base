kapablanka-rbx/android-tools-base

[privacy sandbox] Add targetSdkVersion to DSL and it as manifest merging input
Adds targetSdkVersion option to PrivacySandbox DSL. It will default to compileSdk version if not set.

targetSdkVersion will be provided to the manifest merger to avoid unintentionally setting targetSdkVersion using from dependendency manifests.

Fixes: 345719173
Test: PrivacySandboxSdkTest
Change-Id: Id955e71d5b9256c129a46af2b0c8b9e013ab4497

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/privaysandboxsdk/PrivacySandboxSdkVariantScope.kt
@@ -43,6 +43,7 @@ interface PrivacySandboxSdkVariantScope {
     val mergeSpec: Spec<ComponentIdentifier>
     val compileSdkVersion: String
     val minSdkVersion: ApiVersion
+    val targetSdkVersion: ApiVersion
     val bootClasspath: Provider<List<RegularFile>>
     val bundle: PrivacySandboxSdkBundleImpl
     val services: TaskCreationServices

ouyangsl/android-platform-tools-base

Change-Id: I3fe399797a1c60f740c22cec22e1fade9cae11bb

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/privaysandboxsdk/PrivacySandboxSdkVariantScope.kt
@@ -25,7 +25,6 @@ import com.android.build.gradle.internal.fusedlibrary.FusedLibraryConfigurations
 import com.android.build.gradle.internal.fusedlibrary.FusedLibraryDependencies
 import com.android.build.gradle.internal.publishing.AarOrJarTypeToConsume
 import com.android.build.gradle.internal.services.TaskCreationServices
-import com.android.builder.model.ApiVersion
 import com.android.sdklib.AndroidVersion
 import org.gradle.api.artifacts.component.ComponentIdentifier
 import org.gradle.api.file.ProjectLayout
@@ -44,6 +43,7 @@ interface PrivacySandboxSdkVariantScope {
     val mergeSpec: Spec<ComponentIdentifier>
     val compileSdkVersion: String
     val minSdkVersion: AndroidVersion
+    val targetSdkVersion: AndroidVersion
     val bootClasspath: Provider<List<RegularFile>>
     val bundle: PrivacySandboxSdkBundleImpl
     val services: TaskCreationServices