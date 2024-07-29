da67a5cd321358c77005d7a6ac7a9f79ab49a71a
Sergey Prigogin <sprigogin@google.com>
2024-07-29 14:37:52 -0700

    Add the CharSequence.asSeparatedListContains helper method
    Bug: N/A
    Test: included
    Change-Id: I3fb97d66d8581e236f8eb52aeae3b7f6054a658d

--- common/src/main/java/com/android/utils/StringHelper.kt
+++ common/src/main/java/com/android/utils/StringHelper.kt
@@ -279,3 +279,21 @@ private fun toLineSeparator(separator: String, input: String): String {
     val unixStyle = if (CR.matchesAnyOf(input)) CR.removeFrom(input) else input
     return if (separator == "\n") unixStyle else LF.matcher(unixStyle).replaceAll("\r\n")
 }
+
+/** Checks if the string interpreted as a separated list contains the given element. */
+fun CharSequence.asSeparatedListContains(
+    element: CharSequence,
+    separators: CharSequence = ","
+): Boolean {
+    var offset = 0
+    while (offset < length) {
+        if (startsWith(element, offset) &&
+                (length == offset + element.length ||
+                        separators.contains(this[offset + element.length]))) {
+            return true;
+        }
+        @Suppress("ControlFlowWithEmptyBody")
+        while (offset < length && !separators.contains(this[offset++])) {}
+    }
+    return false
+}
