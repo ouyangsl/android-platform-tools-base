083778813f26c9231a254b2cd339bc272ae66aa1
Tom Renn <tomrenn@google.com>
2024-07-30 18:20:56 +0000

    Add instructions on updating GMaven config in google3
    Bug: 351021086
    Change-Id: I632dbac45a012500baa0e11cdea7319f2b122dca

--- gmaven/test_allowlist.sh
+++ gmaven/test_allowlist.sh
@@ -7,6 +7,10 @@ if [[ "$?" -ne 0 ]]; then
   >&2 echo "Diff detected in maven artifacts!"
   >&2 echo "${output}"
   >&2 echo "-"
+  >&2 echo "IMPORTANT: New artifacts must be added to"
+  >&2 echo "http://google3/configs/production/pod/android-devtools-infra/admrs/service-config/admrs_config.textproto"
+  >&2 echo " See go/gmaven#registering-new-artifacts for more details."
+  >&2 echo "-"
   >&2 echo "Run the following command to update the allowlist:"
   >&2 echo " bazel run //tools/base/gmaven:update-allowlist"
   exit 1
