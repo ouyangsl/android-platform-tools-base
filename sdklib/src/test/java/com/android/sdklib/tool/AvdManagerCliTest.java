88950db42f1fb1b4a02130d65153df73438610ff
jschung <jschung@google.com>
2024-08-07 14:26:57 +0900

    Define new device configurations for automotive
    Created 1408x792 160dpi device configuraiton.
    - Google Play version: Automotive (1408p landscape) with Google Play
    - GoogleAPI version: Automotive (1408p landscape)
    
    Bug: 356017768
    Test: stage automotive system image and check in sdk manager
    Change-Id: I021c254e7da26356189d6b0b0e95c80606d82fdc

--- sdklib/src/test/java/com/android/sdklib/tool/AvdManagerCliTest.java
+++ sdklib/src/test/java/com/android/sdklib/tool/AvdManagerCliTest.java
@@ -399,6 +399,8 @@ public class AvdManagerCliTest {
                 ImmutableList.of(
                         "P automotive_1024p_landscape\n",
                         "P automotive_1080p_landscape\n",
+                        "P automotive_1408p_landscape_with_google_apis\n",
+                        "P automotive_1408p_landscape_with_play\n",
                         "P automotive_distant_display\n",
                         "P automotive_distant_display_with_play\n",
                         "P automotive_large_portrait\n",
@@ -495,11 +497,11 @@ public class AvdManagerCliTest {
                         .join(mLogger.getMessages())
                         .contains(
                                 "P ---------\n"
-                                        + "P id: 65 or \"4in WVGA (Nexus S)\"\n"
+                                        + "P id: 67 or \"4in WVGA (Nexus S)\"\n"
                                         + "P     Name: 4\" WVGA (Nexus S)\n"
                                         + "P     OEM : Generic\n"
                                         + "P ---------\n"
-                                        + "P id: 66 or \"4.65in 720p (Galaxy Nexus)\"\n"
+                                        + "P id: 68 or \"4.65in 720p (Galaxy Nexus)\"\n"
                                         + "P     Name: 4.65\" 720p (Galaxy Nexus)\n"
                                         + "P     OEM : Generic\n"
                                         + "P ---------"));
