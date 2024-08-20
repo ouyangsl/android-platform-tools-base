eb019691a5c6439ee020856bb6e15988d9e5cd04
Daniel LaBar <daniellabar@google.com>
2024-08-20 15:24:03 -0700

    GMD: UtpRunProfile creation for analytics
    Added class to manage the creation of UTP Profiling. As well as wired up
    the UtpRunProfile to UtpTestUtils to allow for the generation of the
    Run Profile
    
    Bug: 330393935
    Test: Added
    Change-Id: Icac1bd80b102a4d5027a4ed814fd55e63e6f6128

---
+++ build-system/gradle-core/src/test/java/com/android/build/gradle/internal/testing/utp/UtpRunProfileTest.kt
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.testing.utp
+
+import com.android.tools.utp.plugins.deviceprovider.profile.proto.DeviceProviderProfileProto
+import com.android.tools.utp.plugins.result.listener.gradle.proto.GradleAndroidTestResultListenerProto.TestResultEvent
+import com.google.common.truth.Truth.assertThat
+import com.google.testing.platform.proto.api.core.TestSuiteResultProto
+import com.google.wireless.android.sdk.stats.DeviceTestSpanProfile
+import com.google.wireless.android.sdk.stats.TestRun
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import java.time.Clock
+import java.time.Instant
+
+class UtpRunProfileTest {
+
+    @get:Rule
+    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @get:Rule
+    var temporaryFolder = TemporaryFolder()
+
+    @Mock
+    lateinit var mockClock: Clock
+
+    @Mock
+    lateinit var testRunResult: UtpTestRunResult
+
+    @Mock
+    lateinit var resultsProto: TestSuiteResultProto.TestSuiteResult
+
+    @Before
+    fun setup() {
+        `when`(testRunResult.resultsProto).thenReturn(resultsProto)
+    }
+
+    private fun setupClock(vararg time: Long) {
+        val iterator = time.toList().iterator()
+        `when`(mockClock.instant()).thenAnswer {
+            Instant.ofEpochMilli(iterator.next())
+        }
+    }
+
+    @Test
+    fun testToProtoUsingDeviceProviderProto() {
+
+        val deviceProfileFolder = temporaryFolder.newFolder()
+        val deviceProfileFile = deviceProfileFolder.resolve("profiling/device1_profile.pb").also {
+            it.parentFile.mkdirs()
+        }
+
+
+        deviceProfileFile.outputStream().use { writer ->
+            DeviceProviderProfileProto.DeviceProviderProfile.newBuilder().apply {
+                deviceProvision = DeviceProviderProfileProto.TimeSpan.newBuilder().apply {
+                    spanBeginMs = 450L
+                    spanEndMs = 550L
+                }.build()
+                deviceRelease = DeviceProviderProfileProto.TimeSpan.newBuilder().apply {
+                    spanBeginMs = 950L
+                    spanEndMs = 1050L
+                }.build()
+            }.build().writeTo(writer)
+        }
+
+        setupClock(
+            100L,
+            250L,
+            350L,
+            650L,
+            850L
+        )
+
+        val metadata = UtpRunProfile.ProfileData(
+            deviceProfileFolder,
+            DeviceTestSpanProfile.DeviceType.VIRTUAL_MANAGED_DEVICE,
+            "device1",
+            UtpRunProfile.TimeSpan(mockClock).apply {
+                recordStart()
+                recordEnd()
+            }
+        )
+
+        val profileProto = UtpRunProfile(
+            metadata,
+            mockClock
+        ).apply {
+            recordSetupStart()
+
+            listener().also {
+                it.onTestResultEvent(
+                    TestResultEvent.newBuilder().apply {
+                        testSuiteStarted = TestResultEvent.TestSuiteStarted.newBuilder().build()
+                    }.build()
+                )
+                it.onTestResultEvent(
+                    TestResultEvent.newBuilder().apply {
+                        testSuiteFinished = TestResultEvent.TestSuiteFinished.newBuilder().build()
+                    }.build()
+                )
+            }
+
+            recordUtpRunFinished(testRunResult)
+        }.toDeviceTestSpanProfileProto()
+
+        assertThat(profileProto.deviceType).isEqualTo(
+            DeviceTestSpanProfile.DeviceType.VIRTUAL_MANAGED_DEVICE
+        )
+        assertThat(profileProto.testKind).isEqualTo(
+            TestRun.TestKind.INSTRUMENTATION_TEST
+        )
+        assertThat(profileProto.processType).isEqualTo(
+            DeviceTestSpanProfile.ProcessType.EXTERNAL_UTP_PROCESS
+        )
+
+        assertThat(profileProto.deviceLockWaitStartTimeMs).isEqualTo(100L)
+        assertThat(profileProto.deviceLockWaitDurationMs).isEqualTo(150L)
+
+        // We do not own setup in UTP so we need to approximate from when we started the UTP process
+        // til the device is requested.
+        assertThat(profileProto.utpSetupStartTimeMs).isEqualTo(350L)
+        assertThat(profileProto.utpSetupDurationMs).isEqualTo(100L)
+
+        assertThat(profileProto.utpProvideDeviceStartTimeMs).isEqualTo(450L)
+        assertThat(profileProto.utpProvideDeviceDurationMs).isEqualTo(100L)
+
+        // We don't own the test setup in UTP so we need to approximate from when the device ended
+        // to when the test is started.
+        assertThat(profileProto.utpTestSetupStartTimeMs).isEqualTo(550L)
+        assertThat(profileProto.utpTestSetupDurationMs).isEqualTo(100L)
+
+        assertThat(profileProto.utpTestRunStartTimeMs).isEqualTo(650L)
+        assertThat(profileProto.utpTestRunDurationMs).isEqualTo(200L)
+
+        assertThat(profileProto.utpTearDownStartTimeMs).isEqualTo(950L)
+        assertThat(profileProto.utpTearDownDurationMs).isEqualTo(100L)
+
+        assertThat(profileProto.progressResult).isEqualTo(
+            DeviceTestSpanProfile.TestProgressResult.TESTS_COMPLETED
+        )
+    }
+}
