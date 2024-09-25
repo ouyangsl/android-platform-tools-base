e6066a4ec29a47feac6bbf3112e0c0aa536ada35
Hannah Chu <hanchu@google.com>
2024-09-25 19:32:44 +0000

    Clean up unused bytecode level
    The bytecode dropdown is currently hidden for all templates, so the only
    value that gets used is the default.
    
    Bug: 367945454
    Test: N/A
    Change-Id: Iea15cab06113b64ee22eb71c312078c2036589d3

--- wizard/template-plugin/src/com/android/tools/idea/wizard/template/TemplateData.kt
+++ wizard/template-plugin/src/com/android/tools/idea/wizard/template/TemplateData.kt
@@ -54,7 +54,6 @@ enum class Language(val string: String, val extension: String) {
 // We define a new enum here instead of reusing existing ones because it should be available
 // both from intellij.android.core and wizardTemplate modules.
 enum class BytecodeLevel(val description: String, val versionString: String) {
-  L8("8", "1.8"),
   L11("11", "11");
 
   override fun toString() = description
