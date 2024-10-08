e4a018462cafaf7e682e0ad9f9c503c84e69d158
Tom Renn <tomrenn@google.com>
2024-10-08 19:49:38 +0000

    Fix atp_test_name typo
    To get invocations ingested into kelloggs.
    
    Change-Id: Ifbe4d5326df64c189bdced3b9799abb3a993812f

--- bazel/ci/studio.py
+++ bazel/ci/studio.py
@@ -104,7 +104,7 @@ def run_bazel_test(
       f'--bes_keywords=b/{build_env.branch}',
       f'--bes_keywords=test_result.build_id/{build_env.build_number}',
       f'--bes_keywords=test_result.build_target/{build_env.build_target_name}',
-      '--bes_keywords=atp_test_name=android_studio/bazel',
+      '--bes_keywords=atp_test_name/android_studio/bazel',
       '--bes_keywords=trigger/BUILD',
       '--build_metadata=cluster=bazel',
       '--build_metadata=run_target=bazel',
